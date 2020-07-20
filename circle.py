e46be3ad2f8ccbf48bf40f90d1084d4d36c934ab
hammy4815 <hammy4815@gmail.com>
2020-07-19 23:14:51 -0700

    fixed some issues

--- circle.py
+++ circle.py
@@ -1,17 +1,21 @@
 class Circle:
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = float(radius)
         self.diameter = radius + radius
         self.area = (radius * radius) * 3.14
-    def setRadius(self, radius):
-        return self.radius
+
+    # def setRadius(self, radius):
+    #     return self.radius        ## pointless
     def area(self, radius):
         return self.area
+
     def diameter(self, radius):
         return self.diameter
+
     def circumference(self, diameter):
-        self.circumference = (3.14 * diameter)
+        self.circumference = 3.14 * diameter
         return self.circumference
-    def __str__ (self):
+
+    def __str__(self):
         self.s1 = str(self.circumference)
         return self.s1
